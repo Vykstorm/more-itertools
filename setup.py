1f21ee4e83c218eb341bd3ad6621c66c32269578
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-08 11:40:01 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+
+'''
+Installation script for this library
+'''
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='more-itertools',
+        version='1.0.0',
+        description='An extension of the standard python module itertools',
+        author='Vykstorm',
+        author_email='victorruizgomezdev@gmail.com',
+        keywords=['itertools', 'iteration', 'recipes'],
+        url='https://github.com/Vykstorm/more-itertools',
+
+        python_requires='>=3.5',
+        install_requires=[],
+        dependency_links=[],
+
+        package_dir={'moreitertools': 'src'},
+        packages=['moreitertools']
+    )
